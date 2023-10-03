0e01a7e4ade3075a2676eec766a2555be49b6958
lululwtv <85116693+lululwtv@users.noreply.github.com>
2023-10-03 10:52:42 +0800

    AddressBook: Add 'remarks' command for enhanced note-taking
    This commit introduces a new 'remarks' command to the AddressBook application, enabling users to add, view, and edit remarks or notes for their contacts. This feature enhances the usability of the AddressBook by providing a convenient way to keep track of important information related to contacts.
    
    Key changes:
    - Added 'remarks' command to the command-line interface (CLI).
    - Implemented functionality to add, view, and edit remarks for individual contacts.
    - Enhanced the display of contact details to include remarks when available.
    - Added tests to ensure the correctness and robustness of the new functionality.
    
    This feature empowers users to maintain better context and information about their contacts, making the AddressBook even more versatile and user-friendly.
    
    Closes #<tutorial>

---
+++ src/main/java/seedu/address/commons/core/Messages.java
@@ -0,0 +1,6 @@
+package seedu.address.commons.core;
+
+public class Messages {
+    public static String MESSAGE_INVALID_COMMAND_FORMAT = "Invalid command format! \n%1$s";
+    public static String MESSAGE_INVALID_PERSON_DISPLAYED_INDEX = "The person index provided is invalid";
+}
